4f084c9511667a7204eba7096a8763e64c62b581
sophyme <poot425@naver.com>
2019-08-06 19:54:44 +0900

    clock

---
+++ clock.js
@@ -0,0 +1,14 @@
+const clockContainer = document.querySelector(".js-clock"),
+ clockTitle = clockContainer.querySelector("h1");
+
+function getTime() {
+    const date = new Date();
+    const minutes = date.getMinutes();
+    const hours = date.getHours();
+    clockTitle.innerText = ``
+}
+
+function init(){
+
+}
+init();

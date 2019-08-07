75bcbe83cf2b843a951312b3806896d6e79305f7
sophyme <46559970+sophyme@users.noreply.github.com>
2019-08-07 14:21:46 +0900

    saving user name1

---
+++ greeting.js
@@ -0,0 +1,25 @@
+const form = document.querySelector(".js-form"),
+  input = form.querySelector("input"),
+  greeting = document.querySelector(".js-greetings");
+
+const USER_LS = "currentUser",
+  SHOWING_CN = "showing";
+
+function painGreeting(text) {
+  form.classList.remove(SHOWING_CN);
+  greeting.classList.add(SHOWING_CN);
+  greeting.innerText = `Hello ${text}`;
+}
+
+function loadName() {
+  const currentUser = localStorage.getItem(USER_LS);
+  if (currentUser === null) {
+  } else {
+    painGreeting(currentUser);
+  }
+}
+
+function init() {
+  loadName();
+}
+init();

97cb4d93fe06327a733f1adcdcd5e0b4b05c4229
sophyme <poot425@naver.com>
2019-08-08 13:33:45 +0900

    greetings is done

--- greeting.js
+++ greeting.js
@@ -5,6 +5,22 @@ const form = document.querySelector(".js-form"),
 const USER_LS = "currentUser",
   SHOWING_CN = "showing";
 
+function saveName(text){
+  localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event){
+  event.preventDefault();
+  const currentValue = input.value;
+  painGreeting(currentValue);
+  saveName(currentValue);
+}
+
+function askForName(){
+  form.classList.add(SHOWING_CN)
+  form.addEventListener("submit", handleSubmit);
+}
+
 function painGreeting(text) {
   form.classList.remove(SHOWING_CN);
   greeting.classList.add(SHOWING_CN);
@@ -14,6 +30,7 @@ function painGreeting(text) {
 function loadName() {
   const currentUser = localStorage.getItem(USER_LS);
   if (currentUser === null) {
+    askForName();
   } else {
     painGreeting(currentUser);
   }

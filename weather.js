20bc546b37e63fbc7ce868f344378bdc7acba87d
sophyme <46559970+sophyme@users.noreply.github.com>
2019-08-11 16:28:04 +0900

    geolocation
    ing...

---
+++ weather.js
@@ -0,0 +1,24 @@
+const COORDS = "coords";
+
+function handleGeoSuccess(position) {
+  console.log(position);
+}
+function handleGeoError() {
+  console.log("Can't access geo location");
+}
+
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+}
+
+function loadCoords() {
+  const loadedCords = localStorage.getItem(COORDS);
+  if (loadedCords === null) {
+    askForCoords();
+  } else {
+    //getweather
+  }
+}
+
+function init() {}
+init();

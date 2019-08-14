e0a307f3b3006077626af8f4622bacc56875f55c
sophyme <46559970+sophyme@users.noreply.github.com>
2019-08-14 13:28:30 +0900

    done

--- weather.js
+++ weather.js
@@ -1,14 +1,21 @@
+const weather = document.querySelector(".js-weather");
+
 const API_KEY = "1b2e0db62cd96a7bf04069b332104fd1";
-const COORDS = 'coords';
+const COORDS = "coords";
 
-function getWeather(lat,lon) {
-  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`).then(function(json){
-    return response.json()
-  }).then(function(json){
-    console.log(json);
-  });
+function getWeather(lat, lon) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+  )
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(json) {
+      const temperature = json.main.temp;
+      const place = json.name;
+      weather.innerText = `${temperature} @ ${place}`;
+    });
 }
-
 
 function saveCoords(coordsObj) {
   localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -21,7 +28,7 @@ function handleGeoSuccess(position) {
     latitude,
     longitude
   };
-  saveCoords(coordsObj); 
+  saveCoords(coordsObj);
   getWeather(latitude, longitude);
 }
 function handleGeoError() {
